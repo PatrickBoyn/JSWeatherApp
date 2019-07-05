e999201a6c208e582842e6450db4d0c3c7e26140
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-05 12:49:19 -0400

    Moved  a few things into another file.

--- JavaScript/weather.js
+++ JavaScript/weather.js
@@ -1,8 +1,9 @@
+const api = new Secret();
+
 class Weather {
   constructor(zip) {
-    // This is a free API key, you can get your own.
-    this.apiKey = '49b3b5cf25012c4aec2789499c2f3cc9';
     this.zip = zip;
+    this.apiKey = api.apiKey();
   }
 
   async getWeather() {
