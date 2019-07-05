a138eb6b12bab01ed7ee18bffec33051a23110d5
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-05 18:15:22 -0400

    Attempting to get the weather icon.

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -7,8 +7,8 @@ function getWeather() {
   weather
     .getWeather()
     .then(results => {
-      // console.log(results.weather[0].description);
-      // ui.paint(results);
+      // console.log(results);
+      ui.paint(results);
     })
     .catch(error => console.log(error));
 }
