039abcadd53acf85b87c25242fce6b5e2b199f40
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-05 16:14:17 -0400

    Added a temperatureString and details selector.

--- JavaScript/ui.js
+++ JavaScript/ui.js
@@ -2,5 +2,7 @@ class UI {
   constructor() {
     this.location = document.getElementById('weather-location');
     this.description = document.getElementById('weather-description');
+    this.temperatureString = document.getElementById('weather-string');
+    this.details = document.getElementById('weather-details');
   }
 }
