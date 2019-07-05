460fd818111663198f5302541780f476954ffc0c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-04 20:55:38 -0400

    Added a zipcode.

--- JavaScript/weather.js
+++ JavaScript/weather.js
@@ -1,6 +1,7 @@
 class Weather {
-  constructor() {
+  constructor(zip) {
     // This is a free API key, you can get your own.
     this.apiKey = '49b3b5cf25012c4aec2789499c2f3cc9';
+    this.zip = zip;
   }
 }
