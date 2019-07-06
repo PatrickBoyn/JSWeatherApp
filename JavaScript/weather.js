c9ca672cc92a58079ccf5f93f1b0d87558a24d6e
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-06 01:20:53 -0400

    Seeing what the response actually is. Also trying to get fahrenheit.

--- JavaScript/weather.js
+++ JavaScript/weather.js
@@ -1,18 +1,18 @@
-const api = new Secret();
+const secret = new Secrets();
 
 class Weather {
   constructor(zip) {
     this.zip = zip;
-    this.apiKey = api.apiKey();
+    this.apiKey = secret.apiKey();
   }
 
   async getWeather() {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?zip=${
         this.zip
-      },us&appid=${this.apiKey}&units=metric`
+      },us&appid=${this.apiKey}&units=fahrenheit`
     );
-
+    console.log(response);
     const responseData = await response.json();
 
     return responseData;
