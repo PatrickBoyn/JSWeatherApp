99c11a4f42e7ac7527d203de54579beb1fe28957
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-06 21:02:03 -0400

    Console logging the responseData, and trying to get the icon to work.

--- JavaScript/weather.js
+++ JavaScript/weather.js
@@ -12,12 +12,20 @@ class Weather {
         this.zip
       },us&appid=${this.apiKey}&units=imperial`
     );
-    console.log(response);
-    const responseData = await response.json();
 
+    const responseData = await response.json();
+    console.log(`getWeather: ${responseData.main}`);
     return responseData;
   }
-
+  async getIcon(icon) {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?zip=${
+        this.zip
+      },us&appid=${this.apiKey}&units=imperia&icon=${icon}`
+    );
+    const responseData = await response.json();
+    console.log(`getIcon: ${responseData.weather[0].icon}`);
+  }
   changeLocation(zipCode) {
     this.zip = zipCode;
   }
