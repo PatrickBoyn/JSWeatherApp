6f1b84cb3092156282dcaccd38824d7a6b9b5cc1
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-06 21:10:31 -0400

    Don't need this function.

--- JavaScript/weather.js
+++ JavaScript/weather.js
@@ -17,15 +17,7 @@ class Weather {
     console.log(`getWeather: ${responseData.main}`);
     return responseData;
   }
-  async getIcon(icon) {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?zip=${
-        this.zip
-      },us&appid=${this.apiKey}&units=imperia&icon=${icon}`
-    );
-    const responseData = await response.json();
-    console.log(`getIcon: ${responseData.weather[0].icon}`);
-  }
+
   changeLocation(zipCode) {
     this.zip = zipCode;
   }
