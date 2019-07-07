c3f8e3ad2f0567ef652cf7963da6b0e563f868d6
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-06 20:12:38 -0400

    Made minor adjustments to the code.

--- JavaScript/ui.js
+++ JavaScript/ui.js
@@ -1,4 +1,4 @@
-const weatherClass = new Weather();
+const secrets = new Secrets();
 
 class UI {
   constructor() {
@@ -14,10 +14,11 @@ class UI {
   }
 
   paint(weather) {
+    this.weatherIcon = JSON.stringify(weather.weather[0].icon);
     this.location.textContent = weather.name;
     this.description.textContent = weather.weather[0].description;
     this.temperatureString.textContent = weather.main.temp;
     // TODO figure out what to do to fix the icon not showing.
-    this.icon.setAttribute('src', weather.weather.icon);
+    this.icon.setAttribute('src', weather.weather[0].icon);
   }
 }
