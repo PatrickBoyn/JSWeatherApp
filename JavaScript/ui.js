d3bb7a72b0a66a5366e2cc76bed9907fe4debddc
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-06 21:10:22 -0400

    Found the icon url.

--- JavaScript/ui.js
+++ JavaScript/ui.js
@@ -21,7 +21,7 @@ class UI {
     // TODO figure out what to do to fix the icon not showing.
     this.icon.setAttribute(
       'src',
-      weatherClass.getIcon('weather.weather[0].icon')
+      `http://openweathermap.org/img/wn/${weather.weather[0].icon}`
     );
     this.humidity.textContent = `Humidity: ${weather.main.humidity}`;
     this.pressure.textContent = `Pressure: ${weather.main.pressure}`;
