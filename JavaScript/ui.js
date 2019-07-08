52d14f71ee38e8326a6d448084f5440ababa2e75
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-08 02:17:51 -0400

    Removed a TODO, and added a percent by the humidity.

--- JavaScript/ui.js
+++ JavaScript/ui.js
@@ -22,12 +22,11 @@ class UI {
     this.location.textContent = weather.name;
     this.description.textContent = weather.weather[0].description;
     this.temperatureString.textContent = weather.main.temp;
-    // TODO figure out what to do to fix the icon not showing.
     this.icon.setAttribute(
       'src',
       `http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`
     );
-    this.humidity.textContent = `Humidity: ${weather.main.humidity}`;
+    this.humidity.textContent = `Humidity: ${weather.main.humidity}%`;
     this.pressure.textContent = `Pressure: ${weather.main.pressure}`;
     this.wind.textContent = `Wind: ${weather.wind.speed}`;
     // My own addition to the
