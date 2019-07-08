21db05c9b077f0307509d3ae0e2d67ab992a5ac1
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-08 00:18:50 -0400

    Removed the console log. It's no longer needed.

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -7,7 +7,6 @@ function getWeather() {
   weather
     .getWeather()
     .then(results => {
-      // console.log(results);
       // ui.paint(results);
     })
     .catch(error => console.log(error));
